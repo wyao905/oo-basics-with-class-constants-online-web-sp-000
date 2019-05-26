1529843c8497b1f3daa3a45d1b14dd84b54b10b5
Warren Yao <warrenyca905@gmail.com>
2019-05-26 04:56:11 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,7 +2,7 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
   
-  BRAND = []
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
@@ -15,6 +15,6 @@ class Shoe
   
   def brand=(brand)
     @brand = brand
-    BRAND << brand
+    BRANDS << brand
   end
 end
